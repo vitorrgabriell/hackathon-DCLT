3ba7ecec00890e28e7015ca7ca065992f9cca17c
vitorrgabriell <vgalmeida10@gmail.com>
2026-07-17 18:12:20 -0300

    fix: force_delete nos repositorios ECR (terraform destroy falhava com repos nao-vazios)

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -3,6 +3,7 @@ resource "aws_ecr_repository" "services" {
 
   name                 = "${var.project_name}/${each.value}-service"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
